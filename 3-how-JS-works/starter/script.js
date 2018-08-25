ba77ced260b55066ff82ead2eba03a6ad4ed76ba
Lloyd <lloyd@dreamatic.digital>
2018-08-25 21:59:51 +0200

    Theoretical lectures

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,6 +1,6 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
-
+/*
 
 
 //only works for function declarations
@@ -39,7 +39,7 @@ console.log(age);
 
 //impotant take-away - you can use execute functions in your code before declaring them
 
-
+*/
 
 
 
@@ -91,4 +91,82 @@ function third() {
 
 
 ///////////////////////////////////////
-// Lecture: The this keyword
+// Hoisting & Execution context
+
+/*
+How code is executed.
+
+Execution context object consists of:
+  - Variable Object (VO)
+  - Scope chain
+  - "This" Variable
+
+//Creation Phase
+  - Creates the VO
+  - Create the scope chain
+  - Determine the value of the 'this' variable
+
+//Execution Phase
+  - Code is ran line by line
+
+  1. The argument object is created, containing all arguments that were passed to the function
+  2. Code is scanned for function declarations; for each function, a property is created in the VO, pointing to the function
+  3. Code is scanned for variable declarations; for each variable, a property is created in the VO, set to undefined
+
+  Points 2 and 3 are considered 'hoisting', where the variables are created before the execution starts.
+  - Functions are already defined
+  - Variables are set to undefined, only defined during execution phase
+
+*/
+
+
+///////////////////////////////////////
+// Scoping chain
+
+/*
+  - Answers to the question: where can we access a certain variable
+  - Each new function creates a scope: A space/environment in which the variables defined are accessible
+  - Lexical scoping: A function within another function gets access to the outer functions
+*/
+
+
+///////////////////////////////////////
+// The 'This' Keyword
+/*
+  - Regular function call - the 'this' keyword points at the global object (window)
+  - Method (function in an object) points at the same object
+  - The 'this' keyword is not assigned a value until a function where it is defined is executed
+
+calculateAge(1985);
+
+function calculateAge(year) {
+  console.log(2016 - year);
+  console.log(this);
+}
+*/
+var john = {
+  name: 'John',
+  yearOfBirth: 1990,
+  calculateAge: function() {
+    console.log(this);
+    console.log(2016 - this.yearOfBirth);   //points to john object
+/*
+    function innerFunction () {
+      console.log(this);                    //points to window
+    }
+    innerFunction ();
+    */
+  }
+
+}
+
+john.calculateAge();
+
+
+var mike = {
+  name: 'Mike',
+  yearOfBirth: 1984
+};
+
+mike.calculateAge = john.calculateAge;      //method borrowing
+mike.calculateAge();
