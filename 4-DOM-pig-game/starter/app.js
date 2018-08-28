938681d1431ae97a6fdb7434801a6e3f59b838f7
Lloyd <lloyd@dreamatic.digital>
2018-08-28 17:16:06 +0200

    Progressing the dice game

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,10 +9,62 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 score = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = math.floor(math.random * 6) + 1;
+//document.querySelector('#current-' + activePlayer).textContent = dice;    //changes the text content only
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';   //changes/adds html
+
+//var x = document.querySelector('#score-0').textContent;   //assigns the text content to the variable // x
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+
+
+//Setting up the event handler - see MDN event reference
+
+/*function btn() {
+  //to add later
+}
+btn();
+*/ //normal way to manage functions, unless you want it to be isolated to an event
+
+document.querySelector('.btn-roll').addEventListener('click', function() {       //event is calling the function. Can add function directly here as anonymous function
+
+      //1. Random number
+      var dice = Math.floor(Math.random() * 6) + 1;
+
+      //2. Display the result
+      var diceDOM = document.querySelector('.dice');
+      diceDOM.style.display = 'block';
+      diceDOM.src = 'dice-' + dice + '.png';
+
+      //3. Update the round score IF the rolled number was NOT a 1
+      if (dice !== 1) {
+        //add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+      } else {
+        //next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        //document.querySelector('.player-0-panel').classList.remove('active');
+        //document.querySelector('.player-1-panel').classList.add('active');
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.dice').style.display = 'none';
+      }
+
+
+});
